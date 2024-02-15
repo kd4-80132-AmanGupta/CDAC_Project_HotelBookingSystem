f8df5920fecf1f4f69c5c46e0609cab0f6289d5e
kmark15 <kunal15markam@gmail.com>
2024-02-16 02:11:37 +0530

    User SignUp Completed

---
+++ myreactapp/src/Auth/Signup.js
@@ -0,0 +1,158 @@
+import React from "react";
+import { useState } from "react";
+//import { Link, useNavigate} from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+const Signup = () => {
+    const [roleId, setRoleId] = useState(7);
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailId, setemailId] = useState("");
+    const [mobileNo, setMobileNo] = useState("");
+  
+    const IsValidate = () => {
+      let isproceed = true;
+      let errormessage = "Please fill the value of ";
+      if (userName === null || setUserName === "") {
+        isproceed = false;
+        errormessage += " Fullname";
+      }
+      if (password === null || password === "") {
+        isproceed = false;
+        errormessage += " Password";
+      }
+      if (emailId === null || emailId === "") {
+        isproceed = false;
+        errormessage += " emailId";
+      }
+  
+      if (!isproceed) {
+        toast.warning(errormessage);
+      } else {
+        if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailId)) {
+        } else {
+          isproceed = false;
+          toast.warning("Please enter a valid emailId");
+        }
+      }
+      return isproceed;
+    };
+  
+    const handleRadioChange = (event) => {
+      setRoleId(Number(event.target.value));
+    };
+  
+    const handlesubmit = (e) => {
+  
+      e.preventDefault();
+      let regobj = {userName, password, emailId, mobileNo, roleId };
+      console.log(regobj);
+      if (IsValidate()) {
+        axios.post("http://localhost:5216/users/SignUp", regobj)
+          .then((res) => {
+            toast.success("Registered successfully.");
+          })
+          .catch((err) => {
+            toast.error("Failed: " + err.message);
+          });
+      }
+    };
+  
+    return (
+      <div>
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card">
+              <div className="card-header">
+                <h1>User Registration</h1>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        Full Name <span className="errmsg">*</span>
+                      </label>
+                      <input
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        Password <span className="errmsg">*</span>
+                      </label>
+                      <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        emailId <span className="errmsg">*</span>
+                      </label>
+                      <input
+                        value={emailId}
+                        onChange={(e) => setemailId(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        Phone <span className="errmsg"></span>
+                      </label>
+                      <input
+                        value={mobileNo}
+                        onChange={(e) => setMobileNo(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        <input
+                          type="radio"
+                          value={7}
+                          checked={roleId === 7}
+                          onChange={handleRadioChange}
+                        />
+                        User
+                      </label>
+                      <label>
+                        <input
+                          type="radio"
+                          value={8}
+                          checked={roleId === 8}
+                          onChange={handleRadioChange}
+                        />
+                        Manager
+                      </label>
+                      <p>Selected Value: {roleId}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card-footer">
+                <button type="submit" className="btn btn-primary">
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Signup;
